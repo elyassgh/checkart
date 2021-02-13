d7f31cd9a268005c9e21a9ac460b470c2ebf611c
elyassgh <elghazi.elyass@gmail.com>
2021-02-13 21:50:57 +0100

    first commit

---
+++ app/src/main/java/irisi/digitalaube/checkart/MainActivity.java
@@ -0,0 +1,27 @@
+package irisi.digitalaube.checkart;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Handler handler = new Handler(Looper.getMainLooper());
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 5000);
+
+    }
+}
